563668d8d6b3ff5c08db3968941f8c5d4a51b57a
Travis Tressler <r.travis.tressler@gmail.com>
2019-10-24 20:37:56 -0400

    Impl class import changed

---
+++ lib/repository/repository_impl.dart
@@ -0,0 +1,18 @@
+
+import 'package:news_demo/models/headlines_res.dart';
+import 'package:news_demo/repository/headlines_service.dart';
+import 'package:news_demo/repository/headlines_service_impl.dart';
+import 'package:news_demo/repository/repository.dart';
+import 'package:rxdart/rxdart.dart';
+
+class RepositoryImpl implements Repository {
+
+  final HeadlinesService _headlinesService = HeadlinesServiceImpl();
+
+  @override
+  Observable<List<Article>> fetchArticles() {
+    return Observable.fromFuture(_headlinesService.fetchHeadlines())
+        .map((res) => res.articles);
+  }
+
+}
