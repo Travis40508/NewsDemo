aac1fa95d5ffae52d6cb45d7ce3e04beebacbfb2
Travis Tressler <r.travis.tressler@gmail.com>
2019-10-27 14:49:37 -0400

    Service impl for searching is finished

--- lib/repository/headlines_service.dart
+++ lib/repository/headlines_service.dart
@@ -1,5 +1,7 @@
 import 'package:news_demo/models/headlines_res.dart';
+import 'package:flutter/foundation.dart';
 
 abstract class HeadlinesService {
   Future<NewsRes> fetchHeadlines();
+  Future<NewsRes> searchNews({@required String query});
 }
--- lib/repository/headlines_service_impl.dart
+++ lib/repository/headlines_service_impl.dart
@@ -1,11 +1,13 @@
 import 'package:news_demo/models/headlines_res.dart';
 import 'package:news_demo/repository/headlines_service.dart';
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 export 'headlines_service.dart';
 import 'package:http/http.dart' show Client;
 
 const String _baseUrl = 'https://newsapi.org/v2';
 const String _topHeadlinesParam = '/top-headlines';
+const String _everythingParam = '/everything';
 const String _apiKeyParam = 'apiKey=a08fb4100ddf4622ba34a2f2faf6367b';
 const String _countryParam = 'country=us';
 
@@ -21,6 +23,17 @@ class HeadlinesServiceImpl implements HeadlinesService {
     final json = jsonDecode(res.body);
 
     print('response for fetchHeadlines - $json');
+
+    return NewsRes.fromJson(json);
+  }
+
+  @override
+  Future<NewsRes> searchNews({@required String query}) async {
+    final String _url = '$_baseUrl$_everythingParam?$_apiKeyParam&$query';
+    final res = await client.get(_url);
+    final json = jsonDecode(res.body);
+
+    print('response for fetchEverything - $json');
 
     return NewsRes.fromJson(json);
   }
