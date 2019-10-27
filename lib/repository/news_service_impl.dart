b201fefb14dc7baaf20730e534dccb7c78fb6999
Travis Tressler <r.travis.tressler@gmail.com>
2019-10-27 16:37:36 -0400

    Removed unnecessary printing

--- lib/repository/news_service_impl.dart
+++ lib/repository/news_service_impl.dart
@@ -10,6 +10,7 @@ const String _topHeadlinesParam = '/top-headlines';
 const String _everythingParam = '/everything';
 const String _apiKeyParam = 'apiKey=a08fb4100ddf4622ba34a2f2faf6367b';
 const String _countryParam = 'country=us';
+const int timeoutDuration = 1000;
 
 
 class NewsServiceImpl implements NewsService {
@@ -19,7 +20,7 @@ class NewsServiceImpl implements NewsService {
   @override
   Future<NewsRes> fetchHeadlines() async {
     final String _url = '$_baseUrl$_topHeadlinesParam?$_countryParam&$_apiKeyParam';
-    final res = await client.get(_url);
+    final res = await client.get(_url).timeout(Duration(milliseconds: timeoutDuration));
     final json = jsonDecode(res.body);
 
     print('response for fetchHeadlines - $json');
@@ -30,10 +31,9 @@ class NewsServiceImpl implements NewsService {
   @override
   Future<NewsRes> searchNews({@required String query}) async {
     final String _url = '$_baseUrl$_everythingParam?$_apiKeyParam&q=$query';
-    final res = await client.get(_url);
+    final res = await client.get(_url).timeout(Duration(milliseconds: timeoutDuration));
     final json = jsonDecode(res.body);
 
-    print('@@@@ - $_url');
     print('response for fetchEverything - $json');
 
     return NewsRes.fromJson(json);
