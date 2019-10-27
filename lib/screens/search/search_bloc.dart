78dac1f7cf319946b5eb412f6dc26dd1b21ff8a8
Travis Tressler <r.travis.tressler@gmail.com>
2019-10-27 14:59:57 -0400

    Timer is implemented. Now to search and populate a list.

--- lib/screens/search/search_bloc.dart
+++ lib/screens/search/search_bloc.dart
@@ -1,5 +1,7 @@
 
 
+import 'dart:async';
+
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 import 'package:news_demo/repository/repository.dart';
 import 'package:news_demo/repository/repository_impl.dart';
@@ -7,7 +9,9 @@ import 'package:flutter/foundation.dart';
 
 class SearchBloc extends Bloc {
 
+  static const int INPUT_DELAY = 500;
   Repository _repository = RepositoryImpl();
+  Timer _debounce;
 
   @override
   void dispose() {
@@ -20,7 +24,12 @@ class SearchBloc extends Bloc {
   }
 
   void onTextChanged(final String query) {
-
+    if (_debounce?.isActive ?? false) {
+      _debounce.cancel();
+    }
+    _debounce = Timer(const Duration(milliseconds: INPUT_DELAY), () {
+      print('search!');
+    });
   }
 
 }
