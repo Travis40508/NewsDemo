c94428d67a7a437fc172d054681fa002f15f811a
Travis Tressler <r.travis.tressler@gmail.com>
2019-10-27 14:53:19 -0400

    Mocking prepared for unit testing. Preparing for timer for searching to reduce API calls

--- lib/screens/search/search_bloc.dart
+++ lib/screens/search/search_bloc.dart
@@ -3,6 +3,7 @@
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 import 'package:news_demo/repository/repository.dart';
 import 'package:news_demo/repository/repository_impl.dart';
+import 'package:flutter/foundation.dart';
 
 class SearchBloc extends Bloc {
 
@@ -13,8 +14,13 @@ class SearchBloc extends Bloc {
     // TODO: implement dispose
   }
 
+  SearchBloc();
+  SearchBloc.withMocks({@required final Repository repository}) {
+    this._repository = repository;
+  }
+
   void onTextChanged(final String query) {
-    
+
   }
 
 }
