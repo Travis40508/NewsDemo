fb87667bbca26eb6529dd0ba5c29adc411ab0b3f
Travis Tressler <r.travis.tressler@gmail.com>
2019-10-27 15:22:54 -0400

    finished adding returned articles to subject

--- lib/screens/search/search_bloc.dart
+++ lib/screens/search/search_bloc.dart
@@ -3,9 +3,11 @@
 import 'dart:async';
 
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:news_demo/models/headlines_res.dart';
 import 'package:news_demo/repository/repository.dart';
 import 'package:news_demo/repository/repository_impl.dart';
 import 'package:flutter/foundation.dart';
+import 'package:rxdart/rxdart.dart';
 
 class SearchBloc extends Bloc {
 
@@ -13,22 +15,26 @@ class SearchBloc extends Bloc {
   Repository _repository = RepositoryImpl();
   Timer _debounce;
 
+  final _searchedArticles = PublishSubject<List<Article>>();
+  Observable<List<Article>> get searchedArticlesStream => _searchedArticles.stream;
+
   @override
   void dispose() {
-    // TODO: implement dispose
+    _searchedArticles.close();
   }
 
   SearchBloc();
   SearchBloc.withMocks({@required final Repository repository}) {
     this._repository = repository;
   }
-
+  
   void onTextChanged(final String query) {
     if (_debounce?.isActive ?? false) {
       _debounce.cancel();
     }
     _debounce = Timer(const Duration(milliseconds: INPUT_DELAY), () {
-      print('search!');
+      _repository.searchNews(query: query)
+          .listen((articles) => _searchedArticles.sink.add(articles), onError: (e) => _searchedArticles.sink.addError(e));
     });
   }
 
